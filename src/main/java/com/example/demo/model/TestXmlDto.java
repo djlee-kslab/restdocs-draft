93269fff17775b80a28e07e3ae2c2b24ce89cc9e
djlee-kslab <djlee@kslab.co.kr>
2022-06-09 07:25:01 +0900

    Element validation 추가

--- src/main/java/com/example/demo/model/TestXmlDto.java
+++ src/main/java/com/example/demo/model/TestXmlDto.java
@@ -1,14 +1,30 @@
 package com.example.demo.model;
 
+import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.Valid;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "root")
+@XmlRootElement(name = "Root")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Builder
 public class TestXmlDto {
 
-    @XmlElement(name = "Parameter")
-    private String parameter;
+    @Length(min = 2, message = "너무 짧소")
+    @XmlElement(name = "Parameters")
+    private String parameters;
 
     @XmlElement(name = "Dataset")
     private String dataset;
+
+    public TestXmlDto() {}
+
+    public TestXmlDto(String parameters, String dataset) {
+        this.parameters = parameters;
+        this.dataset = dataset;
+    }
 }
