a32228bdac0d3c2bcb6f32aeab600f5f6ba3b3a7
djlee-kslab <djlee@kslab.co.kr>
2022-06-09 08:42:32 +0900

    Lombok 어노테이션 확정

--- src/main/java/com/example/demo/model/TestXmlDto.java
+++ src/main/java/com/example/demo/model/TestXmlDto.java
@@ -1,51 +1,49 @@
 package com.example.demo.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
+import lombok.*;
 
-import javax.validation.Valid;
 import javax.xml.bind.annotation.*;
 import java.util.List;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @XmlRootElement(name = "Root")
 @XmlAccessorType(XmlAccessType.FIELD)
-@Builder
-@Setter
 public class TestXmlDto {
 
+    @NonNull
     @XmlElementWrapper(name = "Parameters")
     @XmlElement(name = "Parameter")
     private List<Parameter> parameters;
 
     @XmlElement(name = "Dataset")
     private String dataset;
+//
+//    public TestXmlDto(List<Parameter> parameters, String dataset) {
+//        this.parameters = parameters;
+//        this.dataset = dataset;
+//    }
 
-    public TestXmlDto() {}
-
-    public TestXmlDto(List<Parameter> parameters, String dataset) {
-        this.parameters = parameters;
-        this.dataset = dataset;
-    }
-
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     @XmlRootElement(name = "Parameter")
     @XmlAccessorType(XmlAccessType.FIELD)
-    @Builder
     public static class Parameter {
 
         //    @Length(min = 2, message = "너무 짧소")
+        @NonNull
         @XmlAttribute(name = "id")
         private String id;
 
+        @NonNull
         @XmlValue
         private String value;
-
-        public Parameter() {}
-
-        public Parameter(String id, String value) {
-            this.id = id;
-            this.value = value;
-        }
+//
+//        public Parameter(String id, String value) {
+//            this.id = id;
+//            this.value = value;
+//        }
     }
 }
