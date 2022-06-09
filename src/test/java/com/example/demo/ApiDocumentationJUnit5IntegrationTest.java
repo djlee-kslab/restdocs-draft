c5ea54c325a610a460cc3ca1da34c5d4e7aafb93
djlee-kslab <djlee@kslab.co.kr>
2022-06-09 11:28:16 +0900

    주석 충원

--- src/test/java/com/example/demo/ApiDocumentationJUnit5IntegrationTest.java
+++ src/test/java/com/example/demo/ApiDocumentationJUnit5IntegrationTest.java
@@ -130,30 +130,32 @@ public class ApiDocumentationJUnit5IntegrationTest {
                 .andExpect(status().isCreated())
                 .andDo(document("{method-name}",
 
-                        /* docs의 표시할 sample을 보기 좋게 출력한다. */
+                        /* docs의 표시할 sample을 보기 좋게 처리한다. */
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
 
                         /* relaxed[Request/Response]Fields 를 사용한다. */
                         // 까다롭게 필드를 검사할 수 없지만, xml 형식의 특성상 문서화할 필요 없는 공통부분이 너무 많으므로 감수한다.
                         // Field를 검사+문서화 하지 않을 뿐, sample payload는 .content(xmlPayload)를 기반으로 빌드하므로 영향 없다.
-
-                        // + @재민님-advice; relaxed 내에서 특정 부분만 빡빡하게 검사할 수 있는지 찾아보기
-//                        relaxedRequestPartFields(
-//                                "part", subsectionWithPath("Root/Dataset").type(JsonFieldType.STRING).description("is dataset section")
-//                        ),
-//                        relaxedRequestFields(beneathPath("Root/Dataset"),
-//                                subsectionWithPath("Root/Dataset").type(JsonFieldType.STRING).description("is dataset section")
-//                        ),
                         relaxedRequestFields(
                                 subsectionWithPath("Root/Dataset").type(JsonFieldType.STRING).description("is dataset section")
                         ),
-//                        requestFields(
-//                                beneathPath("Root/Dataset"),
-////                                subsectionWithPath("Root/Dataset[1]").type(JsonFieldType.STRING).description("is dataset section")
-////                                subsectionWithPath("//*").type(JsonFieldType.STRING).description("is dataset section")
-//
-//                        ),
+
+                        /*
+                        relaxedRequestPartFields(
+                                "part", subsectionWithPath("Root/Dataset").type(JsonFieldType.STRING).description("is dataset section")
+                        ),
+                        relaxedRequestFields(beneathPath("Root/Dataset"),
+                                subsectionWithPath("Root/Dataset").type(JsonFieldType.STRING).description("is dataset section")
+                        ),
+                        requestFields(
+                                beneathPath("Root/Dataset"),
+//                                subsectionWithPath("Root/Dataset[1]").type(JsonFieldType.STRING).description("is dataset section")
+//                                subsectionWithPath("//*").type(JsonFieldType.STRING).description("is dataset section")
+
+                        ),
+                         */// + @재민님-advice; relaxed 내에서 특정 부분만 빡빡하게 검사할 수 있는지 찾아보려는 시도 above
+
                         relaxedResponseFields(
                                 fieldWithPath("Root/Dataset").type(JsonFieldType.STRING).description("실질적인 응답이 담겨있다.")
                         )
