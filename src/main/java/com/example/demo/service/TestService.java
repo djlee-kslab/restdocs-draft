cd4ae61611e76a55c043708c75d7986956a23b85
djlee-kslab <djlee@kslab.co.kr>
2022-06-13 10:47:44 +0900

    모킹할 서비스 추가

---
+++ src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.model.TestXmlDto;
+
+/**
+ * payload를 그대로 return하게 작성해뒀지만, mocking해서
+ */
+public class TestService {
+
+    public TestXmlDto testMethod(TestXmlDto xmlPayload) {
+        return xmlPayload;
+    }
+}
+
+/*
+package longterm.service
+
+import longterm.dto.request.AdminPttnCdRequestDTO
+import org.springframework.stereotype.Service
+
+@Service
+class LongtermCaregiverService {
+    // FIXME: 2022/05/23  response로 바꿔야함
+    fun selectAdminPatternCode(condition: AdminPttnCdRequestDTO): AdminPttnCdRequestDTO {
+        return condition
+    }
+}
+ */
