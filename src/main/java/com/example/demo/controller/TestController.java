d0e89d02720d77873f34ba978ee5d7c7518c75bd
djlee-kslab <djlee@kslab.co.kr>
2022-06-13 14:50:46 +0900

    url변경 및 Service 추가

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.model.TestXmlDto;
 import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,10 +18,14 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequestMapping("/test")
 public class TestController {
 
-    TestService testService;
+    private final TestService testService;
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/xml")
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+//    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public ResponseEntity<TestXmlDto> save(@RequestBody @Valid TestXmlDto payload) {
         TestXmlDto result = testService.testMethod(payload);
         return new ResponseEntity<TestXmlDto>(result, HttpStatus.OK);
