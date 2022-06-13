b01d93aa29c0adee425c99b3421619ba9f96224f
djlee-kslab <djlee@kslab.co.kr>
2022-06-13 11:51:14 +0900

    @Service 어노테이션 추가

--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.TestXmlDto;
+import org.springframework.stereotype.Service;
 
 /**
  * payload를 그대로 return하게 작성해뒀지만, mocking해서
  */
+@Service
 public class TestService {
 
     public TestXmlDto testMethod(TestXmlDto xmlPayload) {
