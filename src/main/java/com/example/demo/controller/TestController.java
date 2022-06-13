6de6586e915b4ed097ab307801147863564f68d5
djlee-kslab <djlee@kslab.co.kr>
2022-06-13 10:51:51 +0900

    메서드명 수정

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,37 +1,26 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.CrudInput;
-import com.example.demo.model.TestDto;
 import com.example.demo.model.TestXmlDto;
 import com.example.demo.service.TestService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
-//
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @PostMapping("/json")
-//    public ResponseEntity<TestDto> saveByJson(@RequestBody @Valid TestDto jsonPayload) {
-//        return new ResponseEntity<TestDto>(jsonPayload, HttpStatus.CREATED);
-//    }
 
     TestService testService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/xml")
-    public ResponseEntity<TestXmlDto> saveByXml(@RequestBody @Valid TestXmlDto xmlPayload) {
-        TestXmlDto result = testService.testMethod(xmlPayload);
+    public ResponseEntity<TestXmlDto> save(@RequestBody @Valid TestXmlDto payload) {
+        TestXmlDto result = testService.testMethod(payload);
         return new ResponseEntity<TestXmlDto>(result, HttpStatus.OK);
     }
 }
